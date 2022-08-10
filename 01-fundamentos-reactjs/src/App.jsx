2c7053658f3475d9ca7e65d39a17b942312aab9a
AndersonPGS <andersonpgs2001@hotmail.com>
2022-08-10 14:30:47 -0300

    Componente Sidebar

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,22 +1,29 @@
 import { Header } from "./components/Header"
-import "./global.css"
 import { Post } from "./Post"
+
+import styles from "./App.module.css"
+import { Sidebar } from "./components/Sidebar"
+import "./global.css"
+
 
 export function App() {
   return (
     <>
       <Header />
       
-      <h1>Posts: </h1>
-      
-      <Post
-        author="Anderson Paiva"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis repellendus amet eaque animi, labore deleniti sed provident enim tempore! Odio maxime deleniti libero explicabo animi voluptas, tempore hic blanditiis nemo."
-      />
-      <Post
-        author="Rocketseat"
-        content="Olá Anderson, você foi aceito e agora faz parte da Rocketseat ❤️."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post
+            author="Anderson Paiva"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis repellendus amet eaque animi, labore deleniti sed provident enim tempore! Odio maxime deleniti libero explicabo animi voluptas, tempore hic blanditiis nemo."
+          />
+          <Post
+            author="Rocketseat"
+            content="Olá Anderson, você foi aceito e agora faz parte da Rocketseat ❤️."
+          />
+        </main>
+      </div>
     </>
   )
 }
