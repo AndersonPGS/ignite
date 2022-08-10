829860d888436366c9b8d529918310b3a763c1cb
AndersonPGS <andersonpgs2001@hotmail.com>
2022-08-10 13:45:39 -0300

    CSS modules

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,9 +1,14 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
+import "./styles.css"
 
 export function App() {
   return (
     <>
+      <Header />
+      
       <h1>Posts: </h1>
+      
       <Post
         author="Anderson Paiva"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis repellendus amet eaque animi, labore deleniti sed provident enim tempore! Odio maxime deleniti libero explicabo animi voluptas, tempore hic blanditiis nemo."
