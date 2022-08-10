0d5a08d9a8b222aa57feceace0718fbd9be456e7
AndersonPGS <andersonpgs2001@hotmail.com>
2022-08-10 14:54:54 -0300

    Finalizando Sidebar

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilSimpleLine } from "phosphor-react"
 import styles from "./Sidebar.module.css"
 
 export function Sidebar() {
@@ -8,12 +9,20 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1623479322729-28b25c16b011?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/andersonpgs.png"
+        />
+
         <strong>Anderson Paiva</strong>
         <span>Front End Developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilSimpleLine size={20}/>
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   )
