ede6a9d265400e446f660ae8bed778389ac42b0f
AndersonPGS <andersonpgs2001@hotmail.com>
2022-08-11 11:58:27 -0300

    Formulário de comentários

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -21,6 +21,17 @@ export function Post() {
         <p>🗿 <a href="https://github.com/andersonpgs">github.com/andersonpgs</a></p>
         <p><a href="#">#rocketseat</a></p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário" />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+        
+      </form>
     </article>
   )
 }
