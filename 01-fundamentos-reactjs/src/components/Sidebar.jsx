a14b9cd3d92463b442389601d20fb14e84eb4cdf
AndersonPGS <andersonpgs2001@hotmail.com>
2022-08-12 11:30:12 -0300

    Componente Avatar

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilSimpleLine } from "phosphor-react"
+import { Avatar } from "./Avatar"
 import styles from "./Sidebar.module.css"
 
 export function Sidebar() {
@@ -9,10 +10,7 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1623479322729-28b25c16b011?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/andersonpgs.png"
-        />
+        <Avatar src="https://github.com/andersonpgs.png" />
 
         <strong>Anderson Paiva</strong>
         <span>Front End Developer</span>
