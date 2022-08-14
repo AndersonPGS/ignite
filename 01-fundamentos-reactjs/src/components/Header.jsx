42e81f06209905b56c0a3ddd7ed8a28ab9f6dc63
AndersonPGS <andersonpgs2001@hotmail.com>
2022-08-14 14:29:45 -0300

    Adicionando Key

--- 01-fundamentos-reactjs/src/components/Header.jsx
+++ 01-fundamentos-reactjs/src/components/Header.jsx
@@ -4,7 +4,7 @@ import styles from "./Header.module.css";
 export function Header() {
   return ( 
     <header className={styles.header}>
-      <img src={igniteLogo} alt="Ignite Logo" srcset="" />
+      <img src={igniteLogo} alt="Ignite Logo"/>
       <strong>Ignite Feed</strong>
     </header>
   );
--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -42,15 +42,15 @@ export function Post({ author, publishedAt, content }) {
           </div>
 
         </div>
-        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString}>{publishedDateRelativeToNow}</time>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
       </header>
 
       <div className={styles.content}>
         {content.map(line => {
           if (line.type === "paragraph") {
-            return <p>{line.content}</p>
+            return <p key={line.content}>{line.content}</p>
           } else if (line.type === "link"){
-            return <p><a href={`https://${line.content}`}>{line.content}</a></p>
+            return <p key={line.content}><a href={`https://${line.content}`}>{line.content}</a></p>
           }
         })}
       </div>
@@ -73,7 +73,7 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment content={comment} />
+          return <Comment key={comment} content={comment}/>
         })}
       </div>
     </article>
