3fcfbbd77daa83d1812abfefc62a086e7fe236a3
AndersonPGS <andersonpgs2001@hotmail.com>
2022-08-18 13:04:35 -0300

    Base ToDo created

---
+++ 01-challenge-todo-list/src/App.tsx
@@ -0,0 +1,13 @@
+import styles from "./App.module.css";
+import { Header } from "./components/Header";
+import "./global.css";
+
+export function App() {
+  return (
+    <>
+      <Header />
+
+      <div className={styles.wrapper}></div>
+    </>
+  );
+}
---
+++ 01-challenge-todo-list/src/components/Header.tsx
@@ -0,0 +1,9 @@
+import todoLogo from "../assets/todo-logo.svg";
+import styles from "./Header.module.css";
+export function Header() {
+  return (
+    <div className={styles.header}>
+      <img className={styles.logo} src={todoLogo} alt="ToDo Logo Ignite" />
+    </div>
+  );
+}
