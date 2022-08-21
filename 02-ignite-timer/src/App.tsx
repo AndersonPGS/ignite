efb673b5476e78e3450deaf722a494d1d39b12b0
AndersonPGS <andersonpgs2001@hotmail.com>
2022-08-21 13:05:00 -0300

    Estrutura do ThemeProvider

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,3 +1,11 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
-  return <div className="App"></div>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <div></div>
+      <div></div>
+    </ThemeProvider>
+  );
 }
