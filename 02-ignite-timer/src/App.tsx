8e6ba23506ff545a7b5ae73213be98b7f072528d
AndersonPGS <andersonpgs2001@hotmail.com>
2022-08-21 13:07:13 -0300

    Global Style

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
@@ -6,6 +7,8 @@ export function App() {
     <ThemeProvider theme={defaultTheme}>
       <div></div>
       <div></div>
+
+      <GlobalStyle />
     </ThemeProvider>
   );
 }
